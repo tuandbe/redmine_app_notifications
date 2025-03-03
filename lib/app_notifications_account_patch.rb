db38b38c18e02a419f8363aa4179d735c48bbd51
Emiliano Baum <emilianobaum@gmail.com>
2025-03-03 15:43:38 +0000

    Update app_notifications_account_patch.rb
    actualizo para reemplazar alias_method_chain

--- lib/app_notifications_account_patch.rb
+++ lib/app_notifications_account_patch.rb
@@ -1,23 +1,13 @@
 require_dependency 'my_controller'
 
 module AppNotificationsAccountPatch
-  def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethods)
-
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
-
-      alias_method_chain :account, :in_app_option
-    end
-  end
-
-  module InstanceMethods
-    def account_with_in_app_option
-      account = account_without_in_app_option
-      User.safe_attributes 'app_notification', 'app_notification_desktop'
-      return account
-    end
+  # Sobrescribe el método account para extender su comportamiento.
+  def account
+    result = super  # Llama a la implementación original.
+    User.safe_attributes 'app_notification', 'app_notification_desktop'
+    result
   end
 end
 
-MyController.send(:include, AppNotificationsAccountPatch)
+# En lugar de include, usamos prepend para que este módulo tenga prioridad.
+MyController.prepend(AppNotificationsAccountPatch)
